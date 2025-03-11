4b25d95e0c8b12de261d6683a74293246b331b34
itequinoxlabs1995 <51262983+itequinoxlabs1995@users.noreply.github.com>
2025-03-11 16:30:53 +0530

    Add files via upload

---
+++ Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using MaterialGatePassTacker.Models;
+
+namespace MaterialGatePassTracker.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string subject, string htmlBody, T_Gate_Pass request, List<string> filePaths);
+    }
+}
