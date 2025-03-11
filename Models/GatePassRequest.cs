d037359afe9f8633bbc0fe5ab0ccf6658038bd51
ABDULHANNAN001 <suhailabdulhannan@gmail.com>
2025-03-11 17:07:06 +0530

    Add files via upload

---
+++ Models/GatePassRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaterialGatePassTracker.Models
+{
+    public class GatePassRequest
+    {
+        public int Store_ID { get; set; }
+        public string Action { get; set; } // "Assign" or "Reject"
+    }
+}
---
+++ Models/GatePassStatusRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MaterialGatePassTracker.Models
+{
+    public class GatePassStatusRequest
+    {
+        public int GatePassId { get; set; }
+        public string Action { get; set; }
+        public string? Store { get; set; } // This holds the UserName for reassignment
+    }
+}
