72ab3605275f4d6317978d985a5cb39751507831
ABDULHANNAN001 <suhailabdulhannan@gmail.com>
2025-03-11 17:04:22 +0530

    Add files via upload

---
+++ Models/Store.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaterialGatePassTracker.Models
+{
+    [Table("Store")]
+    public class Store
+    {
+        [Key]
+        public int Store_ID { get; set; }
+        public string? Store_Name_Desc { get; set; }
+        public bool ISactive { get; set; }
+        public string? Created_By { get; set; }
+        public DateTime Created_On { get; set; }
+        public DateTime? Modified_On { get; set; }
+        public string? Modified_By { get; set; }
+        public int PID { get; set; } // Foreign key to T_Project
+    }
+
+}
---
+++ Models/T_Gate_Pass_Status.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaterialGatePassTracker.Models
+{
+    [Table("T_Gate_Pass_Status")]
+    public class T_Gate_Pass_Status
+    {
+        [Key]
+        public int GPSID { get; set; }
+        public int GPID { get; set; }
+        public string? AssignUser { get; set; }
+        public string? AssignStore { get; set; }
+        public string? ReassignUser { get; set; }
+        public string? ReassignStore { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string? Status { get; set; }
+    }
+
+}
