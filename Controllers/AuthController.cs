8a988976f68754733e07db2b83144f2aa9a76ff5
itequinoxlabs1995 <51262983+itequinoxlabs1995@users.noreply.github.com>
2025-03-12 18:56:30 +0530

    Update AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -123,8 +123,9 @@ namespace MaterialGatePassTackerAPI.Controllers
         private static string m_exePath = string.Empty;
         public static void LogWrite(string logMessage, string path)
         {
-            // m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            m_exePath = path;
+           var filepath =new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory())).Root + $@"";
+
+             m_exePath = filepath + path;
             string fullpath = m_exePath + "\\" + "log_" + DateTime.Now.ToString("dd-MMM-yyyy") + ".txt";
 
             if (!File.Exists(fullpath))
